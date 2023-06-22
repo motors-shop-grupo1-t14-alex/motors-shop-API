9bb5088fa8d5ea91815bb6a2af70e99d0c614012
davi <daavisouza.santana@gmail.com>
2023-06-22 18:51:32 -0300

    feat: create password recovery

--- src/controllers/users.controllers.ts
+++ src/controllers/users.controllers.ts
@@ -4,6 +4,8 @@ import createUserService from "../services/users/createUser.service";
 import retriveUserService from "../services/users/retriveUser.servives";
 import updateUserService from "../services/users/updateUser.service";
 import deleteUserService from "../services/users/deleteUser.service";
+import sendEmailResetPasswordService from "../services/users/sendEmail.service";
+import resetPasswordService from "../services/users/resetPassword.service";
 
 const createUserController = async (req: Request, res: Response) => {
     const userData: iCreateUser = req.body;
@@ -29,10 +31,27 @@ const updateUserController = async (req: Request, res: Response) => {
     return res.json(updatedUser)
 }
 
-export const deleteUserController = async (req: Request, res: Response) => {
+const deleteUserController = async (req: Request, res: Response) => {
     await deleteUserService(+req.params.id);
   
     return res.status(204).send();
-  };
+};
 
-export { createUserController, retriveUserController, updateUserController };
+const sendEmailResetPasswordController = async (req: Request, res: Response) => {
+    const { email } = req.body
+
+    await sendEmailResetPasswordService(email)
+
+    return res.json({ message: "token send" })
+}
+
+const resetPasswordController = async (req: Request, res: Response) => {
+    const { password } = req.body
+    const { token } = req.params
+
+    await resetPasswordService(password, token)
+
+    return res.json({message: "password change with success"})
+}
+
+export { createUserController, retriveUserController, updateUserController, deleteUserController, sendEmailResetPasswordController, resetPasswordController };
