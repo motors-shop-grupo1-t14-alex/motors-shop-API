d26d268a3603c3a1be0f6ab9a7f059bc02afb605
davi <daavisouza.santana@gmail.com>
2023-06-23 19:31:21 -0300

    feat: fix bug delete all adverts

--- src/services/users/deleteUser.service.ts
+++ src/services/users/deleteUser.service.ts
@@ -5,21 +5,11 @@ import { iAdvertRepo } from "../../interfaces/adverts.interfaces"
 import { Advert } from "../../entities/adverts.entity"
 
 const deleteUserService = async (userID: number): Promise<void> => {
-    const advertRepository: iAdvertRepo = AppDataSource.getRepository(Advert)
     const userRepository: Repository<User> = AppDataSource.getRepository(User)
 
     const user = await userRepository.findOneBy({
         id: userID
     })
-
-    const advert = Object(await advertRepository.findBy({
-        user: userID
-    }))
-
-    if(advert){
-        await advertRepository.remove(advert)
-    }
-
 
     await userRepository.remove(user!)
 }
