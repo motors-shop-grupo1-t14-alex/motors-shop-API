a447a3415db2c8a4ed24fb5e1570d1bb0d83f639
user <biancavell@hotmail.com>
2023-06-23 10:17:44 -0300

    fix: removed 2 middlewares on advertRoutes.get - /user because they weren't allowing the listing of the cars if their corresponding seller  wasn't logged.

--- src/routers/adverts.routes.ts
+++ src/routers/adverts.routes.ts
@@ -1,17 +1,30 @@
 import { Router } from "express";
-import { createAdvertController, listAdvertsByUserController, listAllAdvertsController, removeAdvertController, updateAdvertController } from "../controllers/adverts.controllers";
+import {
+  createAdvertController,
+  listAdvertsByUserController,
+  listAllAdvertsController,
+  removeAdvertController,
+  updateAdvertController,
+} from "../controllers/adverts.controllers";
 import validateToken from "../middlewares/validateToken.middleware";
 import validateData from "../middlewares/validateData.middleware";
 import { createAdvertSchema, updateAdvertSchema } from "../schemas/adverts.schemas";
 import { validateIfUserIsAdminOrSeller } from "../middlewares/users.middlewares";
 import { validateIfAdvertExists, validateIfUserIsOwnerAdvertOrAdmin } from "../middlewares/adverts.middlewares";
 
-const advertsRoutes: Router = Router()
+const advertsRoutes: Router = Router();
 
-advertsRoutes.post("", validateToken, validateIfUserIsAdminOrSeller, validateData(createAdvertSchema), createAdvertController)
-advertsRoutes.get("", listAllAdvertsController)
-advertsRoutes.patch("/:id", validateToken, validateIfAdvertExists, validateIfUserIsOwnerAdvertOrAdmin, validateData(updateAdvertSchema), updateAdvertController)
-advertsRoutes.delete("/:id", validateToken, validateIfAdvertExists, validateIfUserIsOwnerAdvertOrAdmin, removeAdvertController)
-advertsRoutes.get("/user", validateToken, validateIfUserIsAdminOrSeller, listAdvertsByUserController)
+advertsRoutes.post("", validateToken, validateIfUserIsAdminOrSeller, validateData(createAdvertSchema), createAdvertController);
+advertsRoutes.get("", listAllAdvertsController);
+advertsRoutes.patch(
+  "/:id",
+  validateToken,
+  validateIfAdvertExists,
+  validateIfUserIsOwnerAdvertOrAdmin,
+  validateData(updateAdvertSchema),
+  updateAdvertController
+);
+advertsRoutes.delete("/:id", validateToken, validateIfAdvertExists, validateIfUserIsOwnerAdvertOrAdmin, removeAdvertController);
+advertsRoutes.get("/user", listAdvertsByUserController);
 
-export default advertsRoutes
+export default advertsRoutes;
